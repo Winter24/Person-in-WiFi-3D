5209b870c336c8958241c08bfa2d0ab8e91184d9
Your Name <you@example.com>
2026-04-16 02:35:37 +0700

    more zoom

--- tests/test_render_qualitative_figure.py
+++ tests/test_render_qualitative_figure.py
@@ -378,10 +378,10 @@ class TestRenderQualitativeFigure(unittest.TestCase):
 
         config = module.get_render_style_config()
 
-        self.assertEqual(config['title_font_size'], 11)
-        self.assertEqual(config['footer_font_size'], 7.2)
-        self.assertEqual(config['bounds_min_range'], 0.16)
-        self.assertEqual(config['bounds_margin_scale'], 0.08)
+        self.assertEqual(config['title_font_size'], 22)
+        self.assertEqual(config['footer_font_size'], 14.4)
+        self.assertEqual(config['bounds_min_range'], 0.12)
+        self.assertEqual(config['bounds_margin_scale'], 0.04)
 
 
 if __name__ == '__main__':
